68f8e8eb7e3057db79effd620c01af4091c544cd
Klaus Striessnig <k.striessnig@gmail.com>
2022-01-25 20:44:50 +0100

    force thousand separator

--- DataTableConverter/Assisstant/SQL Functions/ThousandSeparator.cs
+++ DataTableConverter/Assisstant/SQL Functions/ThousandSeparator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,18 @@ namespace DataTableConverter.Assisstant.SQL_Functions
     [SQLiteFunction(Name = "THOUSAND_SEPARATOR", Arguments = 1, FuncType = FunctionType.Scalar)]
     class ThousandSeparator : SQLiteFunction
     {
+        NumberFormatInfo nfi = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+
+        public ThousandSeparator()
+        {
+            nfi.NumberGroupSeparator = ".";
+        }
 
         public override object Invoke(object[] args)
         {
             decimal.TryParse(args[0].ToString(), out decimal result);
             int decimals = BitConverter.GetBytes(decimal.GetBits(result)[3])[2];
-            return result.ToString($"N{decimals}");
+            return result.ToString($"N{decimals}", nfi);
         }
     }
 }
